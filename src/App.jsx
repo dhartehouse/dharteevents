76e55c1d6cea3be4b9139c4f6b29c1b170303e6c
dhartehouse <dhartehouse@users.noreply.github.com>
2025-09-23 02:35:33 -0400

    Add routing for new pages

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import Footer from './components/Footer'
 import HomePage from './pages/HomePage'
 import EventsPage from './pages/EventsPage'
 import EventDetailPage from './pages/EventDetailPage'
+import FacilitatorsPage from './pages/FacilitatorsPage'
+import PromotersPage from './pages/PromotersPage'
+import HousesPage from './pages/HousesPage'
+import BrandsPage from './pages/BrandsPage'
 import CheckoutPage from './pages/CheckoutPage'
 import OrderSuccessPage from './pages/OrderSuccessPage'
 import AccountPage from './pages/AccountPage'
@@ -21,6 +25,10 @@ function App() {
             <Route path="/" element={<HomePage />} />
             <Route path="/events" element={<EventsPage />} />
             <Route path="/events/:slug" element={<EventDetailPage />} />
+            <Route path="/facilitators" element={<FacilitatorsPage />} />
+            <Route path="/promoters" element={<PromotersPage />} />
+            <Route path="/houses" element={<HousesPage />} />
+            <Route path="/brands" element={<BrandsPage />} />
             <Route path="/checkout" element={<CheckoutPage />} />
             <Route path="/order/:id" element={<OrderSuccessPage />} />
             <Route path="/account" element={<AccountPage />} />
